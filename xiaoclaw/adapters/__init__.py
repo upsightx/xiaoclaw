83beb37cfe2d62b853015ef5f47c44137da519e3
upsightx <127651198+upsightx@users.noreply.github.com>
2026-02-24 21:32:29 +0800

    v0.3.0: xiaoclaw/adapters/__init__.py

--- xiaoclaw/adapters/__init__.py
+++ xiaoclaw/adapters/__init__.py
@@ -8,3 +8,11 @@ __all__ = ["FeishuAdapter", "get_feishu_client"]
 def get_telegram_adapter():
     from .telegram import TelegramAdapter
     return TelegramAdapter
+
+def get_discord_adapter():
+    from .discord_adapter import DiscordAdapter
+    return DiscordAdapter
+
+def get_slack_adapter():
+    from .slack_adapter import SlackAdapter
+    return SlackAdapter
